fdb8e4ac92961bebbf9056df847955840b402e9d
Yousufkhalid7 <yousufkhalid1308@gmail.com>
2026-04-17 23:19:57 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const express = require('express')
+const cors = require('cors')
+const { GoogleGenerativeAI } = require('@google/generative-ai')
+const { YoutubeTranscript } = require('youtube-transcript')
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const GenAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+app.post('/summarize', async(req, res) => {
+    try{
+        const { videoId } = req.body;
+
+        const transcriptData = await YoutubeTranscript.fetchTranscript(videoId);
+        const transcript = transcriptData.map(chunk=>chunk.text).join(' ');
+        const model = GenAI.getGenerativeModel({ model: 'gemini-pro' });
+        const prompt = 'You are a helpful assistant. Summarize this Youtube video transcript. Pull out the key points, main topics covered, and important facts mentioned.'
+
+        Transcript:
+        '${transcript}';
+
+        const result = await model.generateContent(prompt);
+        const summary =result.response.text();
+
+        res.json({summary, transcript: transcript.slice(0,500)});
+    }catch(error){
+        console.error('Error:', error.message);
+        res.status(500).json({ error: error.message});
+    }
+    });
+
+    const PORT =process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log('Server running on https://localhost:${PORT}');
+
+    });
